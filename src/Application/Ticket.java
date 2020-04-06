3a8944edf1a5f290fc46ce92da30fb1064cbdfe6
Alefanfi <60267188+Alefanfi@users.noreply.github.com>
2020-04-06 11:50:22 +0200

    Add File

---
+++ src/Application/Ticket.java
@@ -0,0 +1,68 @@
+package Application;
+
+import java.util.Date;
+import java.util.ArrayList;
+
+public class Ticket {
+
+	private String id;
+	
+	private ArrayList<Commit> commitsTicket;
+	
+	private Date oldestDate = null;
+
+	public Ticket(String id) {
+
+		this.id = id;
+		this.commitsTicket = new ArrayList<Commit>();
+		
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void addCommit(Commit commit) {
+		// TODO Auto-generated method stub
+		
+		commitsTicket.add(commit);
+		System.out.println(commitsTicket);
+		
+	}
+	
+	public ArrayList<Commit> getCommitsTicket() {
+		return commitsTicket;
+	}
+
+	public void setCommitsTicket(ArrayList<Commit> commitsTicket) {
+		this.commitsTicket = commitsTicket;
+	}
+
+	public Date findTheOldestDate() {
+		// TODO Auto-generated method stub
+		
+		if(oldestDate == null) {
+
+			for(int i=0;i<commitsTicket.size();i++) {
+				
+				if(oldestDate == null) {
+					oldestDate = commitsTicket.get(i).getDate();
+				}
+				else if(oldestDate.compareTo(commitsTicket.get(i).getDate()) < 0) {
+					oldestDate = commitsTicket.get(i).getDate();
+				}
+				
+			}
+		}
+		
+		//System.out.println(oldestDate);
+		return oldestDate;
+		
+	}
+		
+	
+}
